b7b4f977258b895871c6ca01e575e53ebbbac6e7
yiru00 <eva890706@gmail.com>
2023-03-06 02:15:31 +0800

    Activity/[id].vue
    。收藏、取消收藏、發問、刪除發問、報名、取消報名
      都會showAlert()
    。刪除發問、報名、取消報名會再次詢問

--- public/alert.js
+++ public/alert.js
@@ -1,18 +1,20 @@
 export default {
-    methods: {
-        showAlert(message) {
-            this.$swal.fire({
-              text: message,
-              toast: true,
-              position: "bottom",
-              showConfirmButton: false,
-              showCancelButton: false,
-              background: "#555",
-              color: "#fff",
-              timer: 800,
-              width: "fit-content",
-              padding: "5px",
-            });
-          },
-    }
-  }
+  methods: {
+    showAlert(message) {
+      this.$swal.fire({
+        text: message,
+        toast: true,
+        position: "bottom",
+        showConfirmButton: false,
+        showCancelButton: false,
+        background: "#555",
+        color: "#fff",
+        timer: 800,
+        width: "fit-content",
+        padding: "5px",
+      });
+    },
+
+   
+  },
+};
